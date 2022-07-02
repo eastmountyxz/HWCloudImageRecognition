ce26de9d107476e35ea9f6ee6ec26a464afc8982
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2022-07-02 16:06:03 +0800

    Add files via upload

---
+++ 第42章 文字图像识别/42-5-word.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# By:Eastmount
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#读取原始图像
+img = cv2.imread("word.png" )
+
+#转换成灰度图像
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#中值滤波去除噪声
+median = cv2.medianBlur(gray, 3)
+
+#图像直方图均衡化
+equalize = cv2.equalizeHist(median)
+
+#Sobel算子锐化处理
+sobel = cv2.Sobel(median, cv2.CV_8U, 1, 0, ksize = 3)
+
+#图像二值化处理
+ret, binary = cv2.threshold(sobel, 0, 255,
+                            cv2.THRESH_OTSU+cv2.THRESH_BINARY)
+
+#显示图像
+cv2.imshow('Binary Image', binary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
